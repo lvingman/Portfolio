8d12b6adc47ad180903d093bdbd4800e726b4548
lvingman <r_martin@tutanota.com>
2023-12-12 08:36:00 -0300

    Actualiza portfolio a paginas estaticas

---
+++ Scripts/script.js
@@ -0,0 +1,108 @@
+/*
+
+//Asynchronisity
+function doFirstThing(number, callback) {
+    // Asynchronous operation
+    setTimeout(function() {
+        fadeOut();
+        callback(number);
+    }, 1000);
+}
+
+function doSecondThing(number, callback) {
+    // Asynchronous operation
+    setTimeout(function() {
+        loadPage(number)
+        callback();
+    }, 1000);
+}
+
+function doThirdThing() {
+    fadeIn();
+}
+
+function pageFade(number){
+    doFirstThing(function(number){
+        doSecondThing(number, function(){
+            doThirdThing();
+        });
+    });
+}
+
+
+
+//Fade
+var opacity = 0;
+var intervalID = 0;
+
+function fadeOut(){
+    intervalID = setInterval(hide, 1);
+}
+
+function fadeIn(){
+    intervalID = setInterval(show, 1);
+}
+
+function hide(){
+    var target = document.getElementById("target");
+    opacity = Number(window.getComputedStyle(target).getPropertyValue("opacity"));
+    if(opacity>0){
+        opacity = opacity-2;
+        target.style.opacity = opacity;
+    }
+    else{
+        clearInterval(intervalID);
+    }
+}
+
+function show(){
+    var target = document.getElementById("target");
+    opacity = Number(window.getComputedStyle(target).getPropertyValue("opacity"));
+    if(opacity<1){
+        opacity = opacity+2;
+        target.style.opacity = opacity;
+    }
+    else{
+        clearInterval(intervalID);
+    }
+}
+
+*/
+
+
+
+
+//Load pages
+const targetEl = document.querySelector('.target');
+
+const loadPage = number => {
+    var url = "./../Home/Index.html"
+
+    switch (number){
+        case 1:
+            url = "./../Home/Index.html"
+            break;
+        case 2:
+            url = "./../Home/Proyects.html"
+            break;
+        case 3:
+            url = "./../Home/Skillset.html"
+            break;
+        case 4:
+            url = "./../Home/Education.html"
+            break;    
+            
+    }
+    fetch(url)
+        .then(res => {
+            if(res.ok){
+                return res.text()
+            }
+        })
+        .then(htmlPage => {
+            targetEl.innerHTML = htmlPage;
+
+        })
+    
+
+}
